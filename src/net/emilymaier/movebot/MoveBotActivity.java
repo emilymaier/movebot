f589fae0ee8a1e35cee15fdd87e589e56f140d04
Emily Maier <emily@emilymaier.net>
2015-04-02 00:40:43 -0400

    GPS coordinates demo.

--- src/net/emilymaier/movebot/MoveBotActivity.java
+++ src/net/emilymaier/movebot/MoveBotActivity.java
@@ -1,15 +1,50 @@
 package net.emilymaier.movebot;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MoveBotActivity extends Activity
+public class MoveBotActivity extends Activity implements LocationListener
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private TextView latitude;
+	private TextView longitude;
+	private LocationManager lm;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		latitude = (TextView) findViewById(R.id.latitude);
+		longitude = (TextView) findViewById(R.id.longitude);
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+	}
+
+	@Override
+	public void onLocationChanged(Location location)
+	{
+		latitude.setText(String.valueOf(location.getLatitude()));
+		longitude.setText(String.valueOf(location.getLongitude()));
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras)
+	{
+	}
+
+	@Override
+	public void onProviderEnabled(String provider)
+	{
+	}
+
+	@Override
+	public void onProviderDisabled(String provider)
+	{
+	}
 }
