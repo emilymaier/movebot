d640809dbcfca8f9c4277c5e07cf6b3cd2670421
Emily Maier <emily@emilymaier.net>
2015-04-18 02:46:22 -0400

    Added map, with location track.

--- src/net/emilymaier/movebot/MoveBotActivity.java
+++ src/net/emilymaier/movebot/MoveBotActivity.java
@@ -6,12 +6,24 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MoveBotActivity extends Activity implements LocationListener
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveBotActivity extends Activity implements LocationListener, OnMapReadyCallback
 {
-	private TextView latitude;
-	private TextView longitude;
+	private GoogleMap map;
+	private Polyline line;
+	private List<LatLng> points;
 	private LocationManager lm;
 
 	/** Called when the activity is first created. */
@@ -20,8 +32,21 @@ public class MoveBotActivity extends Activity implements LocationListener
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		latitude = (TextView) findViewById(R.id.latitude);
-		longitude = (TextView) findViewById(R.id.longitude);
+		((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap map)
+	{
+		this.map = map;
+		UiSettings ui = this.map.getUiSettings();
+		ui.setZoomGesturesEnabled(false);
+		ui.setScrollGesturesEnabled(false);
+		ui.setTiltGesturesEnabled(false);
+		ui.setRotateGesturesEnabled(false);
+		PolylineOptions options = new PolylineOptions();
+		line = map.addPolyline(options);
+		points = new ArrayList<>();
 		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 	}
@@ -29,8 +54,10 @@ public class MoveBotActivity extends Activity implements LocationListener
 	@Override
 	public void onLocationChanged(Location location)
 	{
-		latitude.setText(String.valueOf(location.getLatitude()));
-		longitude.setText(String.valueOf(location.getLongitude()));
+		LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+		map.animateCamera(CameraUpdateFactory.newLatLngZoom(ll, 16));
+		points.add(ll);
+		line.setPoints(points);
 	}
 
 	@Override
